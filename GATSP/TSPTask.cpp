dfb2965852770dfe016991dfa5747d46ad989ee0
DENOR_JANES\Janes <denorjanes@gmail.com>
2017-12-14 17:24:24 +0200

    Some refactoring

--- GATSP/TSPTask.cpp
+++ GATSP/TSPTask.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <random>
 #include <time.h>
+#include <assert.h>
 
 using namespace std;
 
@@ -35,12 +36,24 @@ struct Chromosome : public vector<int>
 typedef vector<vector<double>> Distances;
 typedef vector<Chromosome> Population;
 
+// help functions
 string city_name(int i);
-void print_distance_matrix(const vector<string>& cities, const Distances& dist);
-void calc_fitness(Population& popul);
 void print_population(const Population& popul);
+void print_distance_matrix(const vector<string>& cities, const Distances& dist);
+void initialize_all(const string& filename,
+					unsigned int& city_num,
+					unsigned int& generation_num,
+					Distances& dist,
+					Population& popul,
+					vector<string>& cities
+					);
+
+// assist function
 int find_best(const Population& popul);
 int find_worst(const Population& popul);
+
+// algorithm functions
+void calc_fitness(Population& popul);
 Population newGeneration(const Population& old_popul, const Distances& dist);
 Chromosome create_chromo(int i, const Distances& dist);
 Chromosome select_one(Population& popul);
@@ -49,6 +62,7 @@ Chromosome shuffle_crossover(const Chromosome& chromo_a, const Distances& dist);
 Chromosome mutate(const Chromosome& chromo, const Distances& dist);
 
 
+// probabilities
 double const MUTATE = 0.3;
 double const CROSSOVER = 0.6;
 double const SHUFFLE = 0.1;
@@ -58,9 +72,9 @@ void main()
 {
 	srand(time(NULL));
 
-	unsigned int city_num, const generation_num = 1000;
+	unsigned int city_num, generation_num;
 	double best_dist = INFINITY;
-	size_t i, j;
+	
 	Distances dist;
 	Population popul;
 	vector<string> cities;
@@ -69,44 +83,29 @@ void main()
 	// line 1: number of cities
 	// line 2: names of cities
 	// line 3: distance matrix
-	ifstream fin("Distance_Matrix.txt");
-	if (!fin.is_open()) return;
-	else
-	{
-		fin >> city_num;
-
-		cities.resize(city_num);
-		dist.resize(city_num);
-		popul.resize(city_num);
-
-		for (i = 0; i < city_num; ++i) fin >> cities[i];
-
-		for (i = 0; i < city_num; ++i)
-		{
-			dist[i].resize(city_num);
-			for (j = 0; j < city_num; ++j)
-			{
-				fin >> dist[i][j];
-				if (dist[i][j] == 0) dist[i][j] = INFINITY;
-			}
-		}
-	}
-
-	for (i = 0; i < city_num; ++i) popul[i] = create_chromo(i, dist);
-	calc_fitness(popul);
+	initialize_all("Distance_Matrix.txt", city_num, generation_num, dist, popul, cities);
 
 	print_distance_matrix(cities, dist);
 
+	// executing genetic algorithm
 	for (int k = 0; k < generation_num; ++k)
 	{
+		// tracking the best path
 		if (popul[find_best(popul)].totalDistance < best_dist)
 		{
 			best_dist = popul[find_best(popul)].totalDistance;
 			cout << "\nCurrent best distance: " << best_dist << " km" << endl;
 		}
+
+		// creating new generation based on the previous
 		Population temp_popul = newGeneration(popul, dist);
 		popul = move(temp_popul);
+
+		// filling with fitness value ( better fitness - better chance to be chosen )
+
 		calc_fitness(popul);
+
+		// optional output of all chromosomes in generation (slowing down the execution)
 		/*print_population(popul);*/
 	}
 
@@ -119,6 +118,44 @@ void main()
 	}
 	cout << endl;
 	system("pause");
+}
+
+void initialize_all(const string& filename,
+	unsigned int& city_num,
+	unsigned int& generation_num,
+	Distances& dist, Population& popul, vector<string>& cities)
+{
+	cout << "Input the number of generations: ";
+	cin >> generation_num;
+	cout << endl;
+
+	size_t i, j;
+
+	ifstream fin(filename);
+	assert(fin.is_open());
+
+	fin >> city_num;
+
+	cities.resize(city_num);
+	dist.resize(city_num);
+	popul.resize(city_num);
+
+	for (i = 0; i < city_num; ++i) fin >> cities[i];
+
+	for (i = 0; i < city_num; ++i)
+	{
+		dist[i].resize(city_num);
+		for (j = 0; j < city_num; ++j)
+		{
+			fin >> dist[i][j];
+			if (dist[i][j] == 0) dist[i][j] = INFINITY;
+		}
+	}
+
+	for (i = 0; i < city_num; ++i) popul[i] = create_chromo(i, dist);
+	calc_fitness(popul);
+
+	fin.close();
 }
 
 void print_distance_matrix(const vector<string>& cities, const Distances& dist)
@@ -169,9 +206,12 @@ Chromosome create_chromo(int city, const Distances& dist)
 {
 	int size = dist.size(), i_city = city, min = 0;
 	Chromosome chromo;
-
+	
+	// pushing start location to the path
 	chromo.push_back(i_city);
 
+	// initialize first generation using greedy algorthim
+	// choosing locally optimal ( shortest ) path
 	for (size_t i = 1; i < size; ++i)
 	{
 		min = i_city;
@@ -189,7 +229,10 @@ Chromosome create_chromo(int city, const Distances& dist)
 		chromo.push_back(i_city);
 	}
 
+	//calculating the totall distance for comparing pathes
 	chromo.totalDistance += dist[i_city][city];
+
+	// closing the circle with the start location
 	chromo.push_back(city);
 
 	return move(chromo);
@@ -222,12 +265,15 @@ void calc_fitness(Population& popul)
 	size_t i;
 	int fit_best = find_best(popul);
 
+	// summing up the reverse value for distances,
+	// so that the shorter is path - the bigger is value
 	for (i = 0; i < popul.size(); ++i)
 	{
 		fitness_rank.push_back((1.0) / popul[i].totalDistance);
 		max_probability += fitness_rank[i];
 	}
 
+	// getting the percents of each value for every path (shorter paths have bigger probability percent)
 	for (i = 0; i < popul.size(); ++i)
 	{
 		popul[i].fitness = fitness_rank[i] / max_probability;
@@ -263,6 +309,8 @@ Chromosome select_one(const Population& popul)
 
 	int index = 0;
 
+	// every path has its specific percent area
+	// so we are moving area by area until we reach
 	while (choice > 0)
 	{
 		choice -= popul[index].fitness;
@@ -276,6 +324,8 @@ Chromosome select_one(const Population& popul)
 
 Chromosome crossover(const Chromosome& chromo_a, const Chromosome& chromo_b, const Distances& dist)
 {
+	// this crossover is swaping randomly chosen frist part of the chromo_a
+	// and filling the tail by values from chormo_b that doesn't to be appeared in chromo_a part
 	Chromosome newChromo;
 	int i,
 		size = chromo_a.size(),
@@ -292,6 +342,7 @@ Chromosome crossover(const Chromosome& chromo_a, const Chromosome& chromo_b, con
 	}
 	newChromo.push_back(newChromo.at(0));
 	
+	// calculating distance for the path
 	for (i = 0; i < size - 2; ++i)
 	{
 		int i_1 = newChromo.at(i),
@@ -304,6 +355,7 @@ Chromosome crossover(const Chromosome& chromo_a, const Chromosome& chromo_b, con
 
 Chromosome mutate(const Chromosome& chromo, const Distances& dist)
 {
+	// in this mutate function we are swaping the starting point with the randomly chosen value
 	int i, j,
 		size = chromo.size(),
 		shift = rand() % (size - 1);
@@ -314,6 +366,7 @@ Chromosome mutate(const Chromosome& chromo, const Distances& dist)
 	swap(newChromo.at(0), newChromo.at(shift));
 	newChromo.at(size - 1) = newChromo.at(0);
 
+	// calculating the distance
 	for (i = 0; i < size - 2; ++i)
 	{
 		int i_1 = newChromo.at(i),
@@ -326,6 +379,9 @@ Chromosome mutate(const Chromosome& chromo, const Distances& dist)
 
 Chromosome shuffle_crossover(const Chromosome& chromo_a, const Distances& dist)
 {
+	// using value from chomo_a as a indexe in chomo_b
+	// to get the value from chomo_b that will be a index of value to swap
+	// with the starting value
 	Chromosome newChromo;
 	int i, i_1, i_2, size = chromo_a.size();
 	for (i = 0; i < chromo_a.size(); ++i)
@@ -334,6 +390,8 @@ Chromosome shuffle_crossover(const Chromosome& chromo_a, const Distances& dist)
 		i_2 = chromo_a[i_1];
 		newChromo.push_back(chromo_a[i_2]);
 	}
+
+	// calculating distances
 	for (i = 0; i < size - 2; ++i)
 	{
 		i_1 = newChromo.at(i);
@@ -348,17 +406,18 @@ Population newGeneration(const Population& old_popul, const Distances& dist)
 {
 	Population newGener;
 
+	// always adding the best one
 	newGener.push_back(old_popul[find_best(old_popul)]);
 
+	// fill the new population based on the randomly chosen chromoes and actions
 	for (size_t i = 1; i < old_popul.size(); ++i)
 	{
 		Chromosome first_selected = select_one(old_popul),
-			second_selected = select_one(old_popul);
+				   second_selected = select_one(old_popul);
 
 		double choice = (double)((rand() % 100)) / 100;
 
-		if (first_selected == second_selected &&
-			SHUFFLE - choice >= 0)
+		if (first_selected == second_selected && SHUFFLE - choice >= 0)
 		{
 			first_selected = shuffle_crossover(first_selected, dist);
 		}
